640e9318688eeacf7b966b5b3ffa19c17d258961
balderban <maria.perdomo@mocion.com.co>
2025-07-23 02:50:50 -0500

    updating API url

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import type { Product } from "../types/Product";
 
 //const API_URL = "https://hubbell-backend.vercel.app/api/products";
-const API_URL  = "https://hubbell-backend-vmetx.ondigitalocean.app/";
+const API_URL  = "https://hubbell-backend-vmetx.ondigitalocean.app/api/products";
 
 interface ProductsResponse {
   products: Product[];
