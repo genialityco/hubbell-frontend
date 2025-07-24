164597466489e02b7caedbdbeb780e88ded36b87
juanfe <lassojuanfe@gmail.com>
2025-07-24 13:27:10 -0500

    Comenta ruta en app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import ProductDetail from "./pages/ProductDetail";
+// import ProductDetail from "./pages/ProductDetail";
 import Cart from "./pages/Cart";
 import AdminPanel from "./pages/AdminPanel";
 import Header from "./layouts/Header";
@@ -14,7 +14,7 @@ export default function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/product/:code" element={<ProductDetail />} />
+          {/* <Route path="/product/:code" element={<ProductDetail />} /> */}
           <Route path="/cart" element={<Cart />} />
           <Route path="/admin" element={<AdminPanel />} />
         </Routes>
